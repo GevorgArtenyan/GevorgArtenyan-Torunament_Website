5693ee961fca6c0cbd468ab3d27975a450d14cda
GevorgArtenyan <53793770+GevorgArtenyan@users.noreply.github.com>
2020-03-31 21:49:44 -0700

    Now users can add and remove players

--- tournament/tournament/settings.py
+++ tournament/tournament/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tourney',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, r'tourney\static\tourney'),)
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- tournament/tourney/static/tourney/main.js
+++ tournament/tourney/static/tourney/main.js
@@ -1,8 +1,8 @@
 $(function(){
-    for (var i=1; i <=4; i++) {
+    for (var i=1; i <=132; i++) {
     $("#id_player"+i).keyup(function(){
     var sum = ' '
-    for (var n=1; n<=4; n++) {
+    for (var n=1; n<=132; n++) {
     var players = $('#id_player' + n).val()
     p = players.replace(' ', '++')
     sum += p + ' '
@@ -12,4 +12,22 @@ $(function(){
 };
 });
 
+var a = 5
 
+$(function(){
+$('#add_player').click(function(){
+    $("#div_id_player"+a).show()
+    a += 1
+});
+});
+
+$(function(){
+$('#remove_player').click(function(){
+    if (a > 132) {
+    a = 133}
+    if (a >= 6){
+    $("#div_id_player"+ String(parseInt(a - 1))).hide()
+    a -= 1
+}
+});
+});
